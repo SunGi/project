91e21aa69450620bcfc5d66de0edafe40dfe2c8d
SunGi <sunki213@naver.com>
2020-03-01 23:13:19 +0900

    Add a.js

--- a.js
+++ a.js
@@ -13,6 +13,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
+var msg=0;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -20,8 +21,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(route);
-
+app.post('/admingame',(req,res)=>{
+    console.log(req.body.asdf);
+    if(req.body.asdf=='제출'){
+        msg=1;
+    }
+    res.render('./html/frontend/page/page4/quiz.html');
+});
 server.listen(3000, console.log('서버가 열림!'));
 io.on('connection',(socket)=>{
     console.log('socket에 연결됨');
+    io.emit('event_name', msg);
 });
